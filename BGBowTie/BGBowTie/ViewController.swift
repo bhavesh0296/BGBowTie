ac0076987d1362b7faf5b12a201c20ed37b00465
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-06-15 23:57:42 +0530

    Setup the Basic UI and IBOutlets and IBActions

--- BGBowTie/BGBowTie/ViewController.swift
+++ BGBowTie/BGBowTie/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    @IBOutlet weak var wearButton: UIButton!
+    @IBOutlet weak var rateButton: UIButton!
+
+
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+
+
+    // MARK: - IBActions
+
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+
+    }
+
+    @IBAction func wear(_ sender: UIButton) {
+
+    }
+
+    @IBAction func rate(_ sender: UIButton) {
+
     }
 
 
